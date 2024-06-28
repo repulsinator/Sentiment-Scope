8e99e4bba8d7c3a6a4bb763e125905d7be12705e
Tarun <75080636+repulsinator@users.noreply.github.com>
2024-06-28 20:23:48 +0530

    Added CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/sentiment', methods=['POST'])
 def analyze_sentiment():
